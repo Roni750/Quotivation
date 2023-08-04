9ffa57b84239aa6718158639e8944d72c529833c
roni750 <yerushalmironi@gmail.com>
2023-08-04 16:36:07 +0300

    Made layout changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,25 @@ import './assets/style/main.scss'
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { Header } from './components/header'
 import { Footer } from './components/footer'
+import { QuoteIndex } from './pages/quote-index'
+import { Docs } from './pages/docs'
 
 export default function App() {
 
   return (
     <>
       <Router>
-        <Header></Header>
+        <section className="main-layout">
+          <Header></Header>
+          <main>
+            <Routes>
+              <Route element={<QuoteIndex />} path="/" />
+              <Route element={<Docs />} path="/docs" />
+            </Routes>
+          </main>
+          <Footer></Footer>
+        </section>
       </Router>
-      <Footer></Footer>
     </>
   )
 }
