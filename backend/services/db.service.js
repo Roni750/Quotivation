3586d9dd6e8e0e091a59b8a6e5b7850aab4bc61e
Roni750 <YerushalmiRoni@gmail.com>
2023-08-07 19:52:32 +0300

    Added logger

--- backend/services/db.service.js
+++ backend/services/db.service.js
@@ -2,6 +2,7 @@ import mongoDB from 'mongodb'
 const {MongoClient} = mongoDB
 
 import {config} from '../config/index.js'
+import { logger } from './logger.service.js'
 
 export const dbService = {
     getCollection
