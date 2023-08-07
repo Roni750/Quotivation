926981445460f89b3f3efdaf20f7c4413d1301ee
Roni750 <YerushalmiRoni@gmail.com>
2023-08-07 19:51:45 +0300

    Added getQuotesByAuthor API route

--- backend/api/quote/quote.controller.js
+++ backend/api/quote/quote.controller.js
@@ -2,30 +2,48 @@ import { logger } from '../../services/logger.service.js'
 import { quoteService } from './quote.service.js'
 
 export async function getQuoteById(req, res) {
-    try {
-        const quoteId = req.params.id
-        console.log("quoteId", quoteId)
-        const filter = req.query
-        const quote = await quoteService.getById(quoteId)
-        if (quote) res.json(quote)
-        else throw new Error('Failed to get quote')
-    } catch (err) {
-        console.error('Failed to get quote', err.message)
-        res.status(400).send({ error: err.message })
-    }
+  try {
+    const quoteId = req.params.id
+    console.log("quoteId", quoteId)
+    const filter = req.query
+    const quote = await quoteService.getById(quoteId)
+    if (quote) res.json(quote)
+    else throw new Error('Failed to get quote')
+  } catch (err) {
+    console.error('Failed to get quote', err.message)
+    res.status(400).send({ error: err.message })
+  }
+}
+
+export async function getQuotesByAuthor(req, res) {
+  const author = req.params.name
+  try {
+    const quotes = await quoteService.queryByAuthor(author)
+    if (quotes.length >= 1) res.json(quotes)
+    else throw new Error(`Failed to get quotes by ${author}`)
+  } catch (err) {
+    logger.error(`Failed to get quotes by ${author}`)
+    res.status(400).send({ error: err.message })
+  }
+}
+
+export async function getBatchQuotes(req, res) {
+  const amount = req.params.amount
+  console.log(amount)
+  const quotes = await quoteService.getBatchedQuotes(amount)
 }
 
 export async function getQuotes(req, res) {
-    try {
-      logger.debug('Getting Quotes:', req.query)
-      const filterBy = {
-        txt: req.query.txt || '',
-        pageIdx: req.query.pageIdx
-      }
-      const quotes = await quoteService.query(filterBy)
-      res.json(quotes)
-    } catch (err) {
-      logger.error('Failed to get quotes', err)
-      res.status(400).send({ err: 'Failed to get quotes' })
+  try {
+    logger.debug('Getting Quotes:', req.query)
+    const filterBy = {
+      txt: req.query.txt || '',
+      pageIdx: req.query.pageIdx
     }
+    const quotes = await quoteService.query(filterBy)
+    res.json(quotes)
+  } catch (err) {
+    logger.error('Failed to get quotes', err)
+    res.status(400).send({ err: 'Failed to get quotes' })
   }
+}
--- backend/api/quote/quote.service.js
+++ backend/api/quote/quote.service.js
@@ -1,16 +1,14 @@
-import mongodb from 'mongodb';
+import mongodb from 'mongodb'
 import { dbService } from '../../services/db.service.js'
-import { logger } from '../../services/logger.service.js';
-import { MongoClient } from 'mongodb';
-const { ObjectId } = mongodb;
+import { logger } from '../../services/logger.service.js'
+import { MongoClient } from 'mongodb'
+const { ObjectId } = mongodb
 
 async function getById(quoteId) {
     try {
         const collection = await dbService.getCollection('quote')
         const idToScan = new ObjectId(quoteId)
-        const quote = await collection.findOne({ _id: idToScan });
-        console.log("quoteId:", quoteId)
-        console.log("quote:", quote)
+        const quote = await collection.findOne({ _id: idToScan })
         return quote
     } catch (err) {
         console.error(`Error while finding quote ${quoteId}`, err)
@@ -20,17 +18,62 @@ async function getById(quoteId) {
 
 async function query() {
     try {
-        const collection = await dbService.getCollection('quote');
-        const quotes = await collection.find({}).toArray();
-        
+        const collection = await dbService.getCollection('quote')
+        const quotes = await collection.find().toArray()
         if (quotes.length === 0) {
-            return null; // No quotes found in the collection
+            return null
         }
         
-        const randomIndex = Math.floor(Math.random() * quotes.length);
-        const randomQuote = quotes[randomIndex];
-        
-        return randomQuote;
+        const randomIndex = Math.floor(Math.random() * quotes.length)
+        const randomQuote = quotes[randomIndex]
+        return randomQuote  
+    } catch (err) {
+        logger.error('cannot find quotes', err)
+        throw err
+    }
+}
+
+
+// async function getBatchedQuotes(amount) {
+//     try {
+//         const collection = await dbService.getCollection('quote')
+//         const quote = await collection.find().limit(+amount)
+//         return quote
+//     } catch (err) {
+//         console.error(`Error while finding quotes`, err)
+//         throw err
+//     }
+// }
+
+async function getBatchedQuotes(amount) {
+    try {
+        const collection = await dbService.getCollection('quote')
+
+        const quotes = await collection.aggregate([
+            { $sample: { size: +amount } },
+            { $limit: +amount }
+        ]).toArray();
+
+        console.log(quotes);
+        return quotes;
+    } catch (err) {
+        console.error(`Error while finding quotes`, err);
+        throw err;
+    }
+}
+
+// async function queryByAuthor(author) {
+//     const collection = await dbService.getCollection('quote')
+//     const quotes = await collection.find({"author": author}).toArray()
+//     console.log(quotes);
+//     return quotes
+// }
+
+async function queryByAuthor(author) {
+    try {
+        const collection = await dbService.getCollection('quote');
+        const quotes = await collection.find({ author: { $regex: author, $options: 'i' } }).toArray();
+        return quotes;
     } catch (err) {
         logger.error('cannot find quotes', err);
         throw err;
@@ -40,9 +83,10 @@ async function query() {
 // Queries all available quotes
 // async function query() {
 //     try {
-//         // const criteria = {}
+        // const criteria = {}
 //         const collection = await dbService.getCollection('quote')
 //         const quotes = await collection.find({}).toArray()
+//         console.log(quotes)
 //         return quotes
 //     } catch (err) {
 //         logger.error('cannot find quotes', err)
@@ -52,5 +96,7 @@ async function query() {
 
 export const quoteService = {
     getById,
-    query
+    query,
+    getBatchedQuotes,
+    queryByAuthor
 }
