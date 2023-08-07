a3baef52250512d2e4b26607fba031a56285baca
Roni750 <YerushalmiRoni@gmail.com>
2023-08-07 19:52:12 +0300

    Added author & batch API routes

--- backend/api/quote/quote.routes.js
+++ backend/api/quote/quote.routes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { getQuoteById, getQuotes } from './quote.controller.js'
+import { getQuoteById, getQuotes, getBatchQuotes, getQuotesByAuthor } from './quote.controller.js'
 const router = express.Router()
 
 // router.get('/', getQuotes)
 router.get('/', getQuotes)
 router.get('/:id', getQuoteById)
-
+router.get('/batch/:amount', getBatchQuotes)
+router.get('/author/:name', getQuotesByAuthor)
 export const quoteRoutes = router
