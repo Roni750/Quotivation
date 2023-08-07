7b5d0a7a16cd49a6374eca670b62cc1c0460826b
Roni750 <YerushalmiRoni@gmail.com>
2023-08-07 19:52:50 +0300

    Added some documentation content

--- frontend/src/pages/docs.tsx
+++ frontend/src/pages/docs.tsx
@@ -1,6 +1,21 @@
 export function Docs() {
 
     return (
-        <h2>Docs</h2>
+        <section className="docs">
+            <h2>
+                Introduction
+            </h2>
+            Welcome to the API documentation for Quotivation.
+            This API provides motivational quotes to inspire and motivate developers. It allows you to fetch random quotes, search for quotes, and perform other actions related to motivational quotes.
+
+            Base URL
+            The base URL for the API is: http://your-api-base-url.com
+
+            Authentication
+            The API does not require authentication for most endpoints. However, certain advanced features may require API keys or authentication in the future.
+
+            Rate Limits
+            The API has rate limits to prevent abuse and ensure fair usage. The current rate limits are as follows:
+        </section>
     )
 }
