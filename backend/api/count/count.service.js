d147e0b70e10ebba0ed3552c05c51251c4b6f799
Roni750 <YerushalmiRoni@gmail.com>
2023-08-10 19:55:23 +0300

    Removed redundant console logs

--- backend/api/count/count.service.js
+++ backend/api/count/count.service.js
@@ -1,5 +1,5 @@
-// import { logger } from "../../services/logger.service"
 import { dbService } from "../../services/db.service.js"
+import { logger } from "../../services/logger.service.js"
 
 async function query() {
         try {
@@ -9,32 +9,31 @@ async function query() {
             if (usage.length === 0) {
                 return null
             }
-            incrementCount(collection)
 
             return usage
         } catch (err) {
-            console.error('cannot find usage count', err)
+            logger.error('cannot find usage count', err)
             throw err
         }
 }
 
-async function incrementCount(collection) {
+async function incrementCount() {
     try {
-      const usage = await collection.findOne(); // Assuming you want to update a single document
+      const collection = await dbService.getCollection('count')
+      const usage = await collection.findOne()
   
       if (!usage) {
-        return null;
+        return null
       }
   
-      const newCount = usage.count + 1;
+      const newCount = usage.count + 1
       
-      await collection.updateOne({ _id: usage._id }, { $set: { count: newCount } });
-  
-      console.log('hey', usage);
-      return usage;
+      await collection.updateOne({ _id: usage._id }, { $set: { count: newCount } })
+
+      return usage
     } catch (err) {
-      console.error("Couldn't increment usage count", err);
-      throw err;
+      logger.error("Couldn't increment usage count", err)
+      throw err
     }
   }
   
