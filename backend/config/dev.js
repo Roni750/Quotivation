9570cbc8ea8260b51b0cd59e3323e32b97179342
Roni750 <YerushalmiRoni@gmail.com>
2023-08-10 19:52:53 +0300

    Switched off from remote db to local

--- backend/config/dev.js
+++ backend/config/dev.js
@@ -1,4 +1,5 @@
 export default {
-	dbURL: 'mongodb+srv://roni750:roni42@cluster0.ckri6ih.mongodb.net/',
+	// dbURL: 'mongodb+srv://roni750:roni42@cluster0.ckri6ih.mongodb.net/',
+	dbURL: 'mongodb://127.0.0.1:27017/quotes_db',
 	dbName: 'quotes_db',
 }
