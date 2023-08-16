72a23aa1aeb82f7be4fe939bb95918c22eaeb75b
roni750 <yerushalmironi@gmail.com>
2023-08-16 08:32:43 +0300

    Made MongoDB connection modifications

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -2,7 +2,6 @@ import { config } from "dotenv"
 config()
 
 export default {
-	// dbURL: 'mongodb+srv://roni750:roni42@cluster0.ckri6ih.mongodb.net/',
 	dbURL: process.env.MONGODB_URI,
 	dbName: 'quotes_db',
 }
