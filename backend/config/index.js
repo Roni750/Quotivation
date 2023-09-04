bf0b046a1ce7f7761429270bdf9a482b63473b73
roni750 <yerushalmironi@gmail.com>
2023-09-04 12:02:38 +0300

    Made DB connection corrections

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,16 +1,11 @@
 import configProd from './prod.js'
 import configDev from './dev.js'
 
-export var config
+export let config
 
-if (3 === 3) {
+if (process.env.NODE_ENV === 'production') {
     config = configProd
 } else {
     config = configDev
 }
-// if (process.env.NODE_ENV === 'production') {
-//     config = configProd
-// } else {
-//     config = configDev
-// }
 config.isGuestMode = true
--- backend/config/prod.js
+++ backend/config/prod.js
@@ -2,7 +2,6 @@ import { config } from "dotenv"
 config()
 
 export default {
-	dbURL: 'mongodb+srv://roni750:e6l5Dp0t6qNDrbiX@quotescluster.z93crdh.mongodb.net/?retryWrites=true&w=majority',
-	// dbURL: process.env.MONGODB_URI,
+	dbURL: process.env.MONGODB_URI,
 	dbName: 'quotes_db',
 }
