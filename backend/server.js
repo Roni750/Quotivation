f3306945d7699cbb618dfb0f2185da2858f51ac4
roni750 <yerushalmironi@gmail.com>
2023-09-07 17:18:54 +0300

    Prettyfied the code

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import http from 'http'
 import cors from 'cors'
 import express from 'express'
-import { logger } from './services/logger.service.js'
 import path from 'path'
+import { logger } from './services/logger.service.js'
 import { config } from 'dotenv'
 import { rateLimit } from 'express-rate-limit'
 
@@ -17,6 +17,19 @@ config()
 
 const app = express()
 const server = http.createServer(app)
+
+const apiLimiter = rateLimit({
+	windowMs: 1 * 60 * 1000,
+	max: 10,
+	keyGenerator: (req) => {
+	    return req.socket.remoteAddress
+	    // return req.ip;
+	},
+	message: "Too many requests from this IP",
+	handler: (req, res) => {
+		res.status(429).json({ error: 'Rate limit exceeded' })
+	},
+})
 
 app.use(cors())
 app.use('/api/quote', apiLimiter)
@@ -35,19 +48,6 @@ if (process.env.NODE_ENV === 'production') {
 	}
 	app.use(cors(corsOptions))
 }
-
-const apiLimiter = rateLimit({
-	windowMs: 1 * 60 * 1000,
-	max: 10,
-	keyGenerator: (req) => {
-	    return req.socket.remoteAddress;
-	    // return req.ip;
-	},
-	message: "Too many requests from this IP",
-	handler: (req, res) => {
-		res.status(429).json({ error: 'Rate limit exceeded' });
-	},
-});
 
 import { quoteRoutes } from './api/quote/quote.routes.js'
 import { countRoutes } from './api/count/count.routes.js'
