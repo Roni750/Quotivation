2ca30ef27b94cb71c3d6a26dbcc1c09ae307ec69
roni750 <yerushalmironi@gmail.com>
2023-09-07 13:49:55 +0300

    Modified request-ip pkg implementation

--- backend/server.js
+++ backend/server.js
@@ -5,22 +5,9 @@ import { logger } from './services/logger.service.js'
 import path from 'path'
 import { config } from 'dotenv'
 import { rateLimit } from 'express-rate-limit'
-import { Request } from 'request-ip'
+const requestIp = require('request-ip');
 
 config()
-
-const apiLimiter = rateLimit({
-	windowMs: 1 * 60 * 1000, // 1 minute
-	max: 10, // Limit each IP to 10 requests per minute
-	keyGenerator: (req) => {
-	    // Use the user's IP address as the key for rate limiting
-	    return req.ip;
-	},
-	message: "Too many requests from this IP",
-	handler: (req, res) => {
-		res.status(429).json({ error: 'Rate limit exceeded' });
-	},
-});
 
 // const apiLimiter = rateLimit({
 // 	windowMs: 1 * 60 * 1000, // 1 minute
@@ -33,7 +20,7 @@ const app = express()
 const server = http.createServer(app)
 
 app.use(cors())
-app.use(request.windowMs)
+app.use(requestIp.mw)
 app.use('/api/quote', apiLimiter)
 app.use(express.json())
 app.use(express.static('public'))
@@ -50,6 +37,18 @@ if (process.env.NODE_ENV === 'production') {
 	}
 	app.use(cors(corsOptions))
 }
+
+const apiLimiter = rateLimit({
+	windowMs: 1 * 60 * 1000,
+	max: 10,
+	keyGenerator: (req) => {
+	    return req.ip;
+	},
+	message: "Too many requests from this IP",
+	handler: (req, res) => {
+		res.status(429).json({ error: 'Rate limit exceeded' });
+	},
+});
 
 import { quoteRoutes } from './api/quote/quote.routes.js'
 import { countRoutes } from './api/count/count.routes.js'
