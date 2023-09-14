92bd427a9e6559fea78325163819a53621cc1bdb
roni750 <yerushalmironi@gmail.com>
2023-09-14 11:41:45 +0300

    Added rate-limiting by IP middleware

--- backend/server.js
+++ backend/server.js
@@ -4,27 +4,13 @@ import express from 'express'
 import path from 'path'
 import { logger } from './services/logger.service.js'
 import { config } from 'dotenv'
-import { rateLimit } from 'express-rate-limit'
+import { rateLimiterUsingThirdParty } from './middleware';
 
 config()
 
 const app = express()
 const server = http.createServer(app)
-const ipLimiters = new Map();
-const createIpLimiter = (ip) => {
-	return rateLimit({
-		windowMs: 1 * 60 * 1000, // 1 minute
-		max: 10, // Maximum 10 requests per minute per IP
-		keyGenerator: (req) => {
-			console.log("ip", ip)
-			return ip; // Use the IP address as the key for rate limiting
-		},
-		message: "Too many requests from this IP",
-		handler: (req, res) => {
-			res.status(429).json({ error: 'Rate limit exceeded' });
-		},
-	});
-};
+
 // const apiLimiter = rateLimit({
 // 	windowMs: 1 * 60 * 1000,
 // 	max: 10,
@@ -38,18 +24,7 @@ const createIpLimiter = (ip) => {
 // })
 
 app.use(cors())
-// app.use('/api/quote', apiLimiter)
-app.use('/api/quote', (req, res, next) => {
-	const clientIp = req.socket.remoteAddress;
-	console.log("clientIp", clientIp)
-	// Check if a rate limiter for this IP already exists; if not, create one
-	if (!ipLimiters.has(clientIp)) {
-		ipLimiters.set(clientIp, createIpLimiter(clientIp));
-	}
-
-	// Apply the rate limiter for this IP
-	ipLimiters.get(clientIp)(req, res, next);
-});
+app.use('/api/quote', rateLimiterUsingThirdParty)
 app.use(express.json())
 app.use(express.static('public'))
 
